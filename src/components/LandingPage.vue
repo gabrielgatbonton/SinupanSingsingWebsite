42074bbeb8d2d508e40fa41af8705d12706f0cc0
Gabriel Gatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2023-08-09 16:11:57 +0800

    Initial commit

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container>
+        <v-row no-gutters>
+            <v-col cols="7">
+                <v-container class="px-0">
+                    <v-row>
+                        <v-col cols="12" class="px-0">
+                            <v-card rounded="lg" flat class="bg-grey-lighten-5">
+                                <v-card-text>Hello World</v-card-text>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const data = ref([])
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-responsive>
+      <v-img
+        class="mx-auto"
+        max-width="500"
+        src="@/assets/SinupanSingsing.jpg"
+      ></v-img>
+    </v-responsive>
+    <div class="mx-10">
+      <v-tabs v-model="tab">
+        <v-tab value="home">Aláya (Home)</v-tab>
+        <v-tab value="two">Íkami (About Us)</v-tab>
+        <v-tab value="three">Kagánápan (Events)</v-tab>
+        <v-tab>Kasulatan (Articles)</v-tab>
+      </v-tabs>
+      <v-window class="" v-model="tab">
+        <v-window-item value="home"> <LandingPage /> </v-window-item>
+
+        <v-window-item value="two"> Two </v-window-item>
+
+        <v-window-item value="three"> Three </v-window-item>
+      </v-window>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import LandingPage from "@/components/LandingPage.vue";
+import { ref } from "vue";
+const tab = ref(null);
+</script>
