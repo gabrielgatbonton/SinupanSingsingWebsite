79ef11d6fe4bc9dadc7c04a091e50eb90750b724
Gabriel Gatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2023-08-09 17:07:11 +0800

    Updated Component

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -5,8 +5,15 @@
                 <v-container class="px-0">
                     <v-row>
                         <v-col cols="12" class="px-0">
-                            <v-card rounded="lg" flat class="bg-grey-lighten-5">
-                                <v-card-text>Hello World</v-card-text>
+                            <v-card rounded="lg" flat class="bg-grey-lighten-3">
+                              <v-container>
+                                <v-row no-gutters>
+                                    <v-col cols="auto">
+                                        <v-btn variant="plain">Articles</v-btn>
+                                        <div class="ml-3 text-h4">{{ value1 }}</div>
+                                    </v-col>
+                                </v-row>
+                              </v-container>
                             </v-card>
                         </v-col>
                     </v-row>
@@ -17,8 +24,17 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const data = ref([])
+import { reactive, ref } from 'vue';
+// Create a reactive object using the reactive function
+const data = reactive({
+  title: ref("Kapampángan Language is “Definitely Endangered” - UNESCO"),
+  // ... other variables
+});
+
+// You can access the variables like this:
+const value1 = data.title;
+
+
 </script>
 
 <style scoped>
